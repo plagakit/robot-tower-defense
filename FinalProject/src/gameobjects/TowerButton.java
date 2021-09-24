05930617ad79168037908ac662543f5b6a1b70f3
350076576 <350076576@408X0221736BQ22.EDU.YRDSB.NET>
2021-09-24 14:20:05 -0400

    Added TowerButton

---
+++ FinalProject/src/gameobjects/TowerButton.java
@@ -0,0 +1,28 @@
+package gameobjects;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import general.Vector2;
+import scenes.GameScene;
+
+public class TowerButton extends Button {
+
+	public TowerButton(GameScene scene, String name, Vector2 pos) {
+		super(scene, name, pos);
+		
+	}
+
+	@Override
+	protected void onClick() {
+		
+
+	}
+
+	@Override
+	public void render(Graphics2D g) {
+		g.setColor(Color.BLUE);
+		g.fillOval((int)pos.x, (int)pos.y, 50, 50);
+	}
+
+}
