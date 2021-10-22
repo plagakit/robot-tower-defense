07a110b3a785c1205ab95764da5a8c8533d9771c
350076576 <350076576@408X0221736BQ22.EDU.YRDSB.NET>
2021-10-22 13:41:28 -0400

    Change robot pierce to 2

--- FinalProject/src/towers/Robot.java
+++ FinalProject/src/towers/Robot.java
@@ -9,7 +9,7 @@ import scenes.GameScene;
 public class Robot extends Tower {
 	
 	public Robot(GameScene scene, Vector2 pos) {
-		super(scene, "Robot", pos, 1, 1, 1000,
+		super(scene, "Robot", pos, 1, 2, 1000,
 				new BuyInfo("TEST", "This is a test description. Wow!", 100));
 		
 		sprite = scene.getGame().getSpriteManager().getSprite("robot.png");
