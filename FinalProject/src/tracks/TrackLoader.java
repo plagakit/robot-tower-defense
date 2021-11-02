d7b7b24770153121409283005d4ba4d141910455
wer8888 <wer8888vg@gmail.com>
2021-11-02 18:51:54 -0400

    Add track loader

---
+++ FinalProject/src/tracks/TrackLoader.java
@@ -0,0 +1,57 @@
+package tracks;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class TrackLoader {
+	
+	private HashMap<String, TrackData> trackList;
+	
+	public TrackLoader() {
+		trackList = new HashMap<>();
+		loadTracks();
+		loadCustomTracks();
+	}
+	
+	private void loadTracks() {
+		String dataPath = "imagedata.txt";
+		Scanner sc = new Scanner(ClassLoader.getSystemClassLoader().getResourceAsStream(dataPath));
+		
+		while (sc.hasNextLine()) {
+			String path = sc.nextLine();
+			
+			if (path.toCharArray()[0] == '#')
+				continue;
+			
+			System.out.println("Loading track " + path);
+			
+			try {
+				ObjectInputStream objIn = new ObjectInputStream(ClassLoader.getSystemResourceAsStream(path));
+				TrackData readData = (TrackData)objIn.readObject();
+				System.out.println(readData);
+				System.out.println(readData.getBackground());
+				System.out.println(readData.getMask());
+				System.out.println(readData.getPoints());
+				objIn.close();
+				
+				trackList.put(path, readData);
+			} catch (IOException | ClassNotFoundException e) { e.printStackTrace(); }
+
+		}
+		
+		sc.close();
+	}
+	
+	private void loadCustomTracks() {
+		//TODO MAKE THIS
+	}
+	
+	public TrackData get(String name) {
+		TrackData data = trackList.get(name);
+		if (data == null)
+			System.out.println("Warning: track data " + name + " is null.");
+		return data;
+	}
+}
