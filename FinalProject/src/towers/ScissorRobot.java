7c4924d56099bfe743ad7e10387c0e3a84db48c4
wer8888 <wer8888vg@gmail.com>
2021-11-04 01:13:31 -0400

    Correct scissor movement

--- FinalProject/src/towers/ScissorRobot.java
+++ FinalProject/src/towers/ScissorRobot.java
@@ -10,8 +10,8 @@ import scenes.GameScene;
 public class ScissorRobot extends Tower {
 
 	public ScissorRobot(GameScene scene, Vector2 pos) {
-		super(scene, "ScissorRobot", pos, 1, 4, 750, 
-				new BuyInfo("Scissor Robot", "This is a test description. Wow!", 350));
+		super(scene, "ScissorRobot", pos, 1, 5, 750, 
+				new BuyInfo("Scissor Robot", "High pierce, but needs careful positioning.", 350));
 		sprite = scene.getGame().getSpriteManager().getSprite("scissorrobot.png");
 	}
 
@@ -19,7 +19,7 @@ public class ScissorRobot extends Tower {
 	protected void fire(Bloon target) {
 		rotation = Vector2.lookAtAngle(pos, target.getPos()) + 90;
 		
-		Scissor p = new Scissor(scene, pos, target.getPos(), "scissor.png", damage, pierce, 750);
+		Scissor p = new Scissor(scene, pos, target.getPos(), "scissor.png", damage, pierce);
 		scene.getProjectiles().add(p);
 	}
 
