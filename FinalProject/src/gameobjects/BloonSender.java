91945cbead18e7f2896dbfb6b4e69c0a73a3127d
350076576 <350076576@408X0221736BQ22.EDU.YRDSB.NET>
2021-11-04 14:12:32 -0400

    Barebones bloon sender

---
+++ FinalProject/src/gameobjects/BloonSender.java
@@ -0,0 +1,37 @@
+package gameobjects;
+
+import general.Timer;
+import scenes.GameScene;
+
+public class BloonSender {
+
+	private GameScene scene;
+	private Timer bspawntimer;
+	
+	private boolean sending;
+	
+	public BloonSender(GameScene scene) { 
+		this.scene = scene;
+		
+		sending = false;
+	}
+	
+	
+	
+	public void update() { 
+		if (sending) {
+			bspawntimer.update();
+			if (bspawntimer.isDone()) {
+				Bloon b = new Bloon(scene, scene.getTrack().getPoints()[0], BloonType.GREEN);
+				scene.getBloons().add(b);
+				bspawntimer.restart();
+			}
+		}
+	}
+	
+	public void sendRound(int round) { 
+		sending = true;
+		bspawntimer = new Timer(scene.getGame(), 500);
+	}
+	
+}
--- FinalProject/src/tracks/Wave.java
+++
@@ -1,5 +0,0 @@
-package tracks;
-
-public class Wave {
-
-}
