dc1833314920ceb18e4658079c9133be93e79b90
wer8888 <wer8888vg@gmail.com>
2021-11-06 21:46:22 -0400

    Remove speed modifier from projectiles

--- FinalProject/src/projectiles/Pellet.java
+++ FinalProject/src/projectiles/Pellet.java
@@ -5,15 +5,13 @@ import scenes.GameScene;
 
 public class Pellet extends Projectile {
 
-	public static final float DEFAULT_SPEED = 5;
-	private float speed;
+	public static final float SPEED = 5;
 	
-	public Pellet(GameScene scene, Vector2 pos, Vector2 target, String spritePath, int damage, int pierce, float speed, int despawnTime) {
+	public Pellet(GameScene scene, Vector2 pos, Vector2 target, String spritePath, int damage, int pierce, int despawnTime) {
 		super(scene, "Pellet", pos, spritePath, damage, pierce, despawnTime);
-		this.speed = speed;
 		
 		Vector2 direction = Vector2.direction(pos, target);
-		vel = Vector2.multiply(direction, speed);
+		vel = Vector2.multiply(direction, SPEED);
 		rotation = Vector2.lookAtAngle(pos, target);
 	}
 
--- FinalProject/src/towers/Robot.java
+++ FinalProject/src/towers/Robot.java
@@ -24,7 +24,7 @@ public class Robot extends Tower {
 		rotation = Vector2.lookAtAngle(pos, target) + 90;
 		
 		Pellet p = new Pellet(scene, pos, target, 
-				"pellet.png", damage, pierce, Pellet.DEFAULT_SPEED, Pellet.DEFAULT_DESPAWN_TIME);
+				"pellet.png", damage, pierce, Pellet.DEFAULT_DESPAWN_TIME);
 		scene.getProjectiles().add(p);
 	}
 	
