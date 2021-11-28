a560a20145b0cf7131958d855aa89ca15c3527b2
wer8888 <wer8888vg@gmail.com>
2021-11-28 15:06:47 -0500

    Add barebones upgrade path class

---
+++ FinalProject/src/towers/UpgradePath.java
@@ -0,0 +1,35 @@
+package towers;
+
+public class UpgradePath {
+	
+	private Upgrade u0_1, u;
+
+
+	
+	/*
+	 * Upgrade path representation: 
+	 *       End
+	 *    1-2   2-1
+	 *   /   \ /   \
+	 * 0-2   1-1   2-0
+	 *   \   / \   /
+	 *    0-1   1-0
+	 *      \   /
+	 *       0-0
+	 *      Start                
+	 */
+	
+	
+	public UpgradePath(Upgrade[] left, Upgrade[] right) {
+
+	}
+	
+	public void advanceLeft() {
+
+	}
+	
+	public void advanceRight() {
+		
+	}
+	
+}
