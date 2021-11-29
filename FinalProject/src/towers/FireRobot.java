2d178c35851743fe6f21db3bf5153e2b986d45c4
wer8888 <wer8888vg@gmail.com>
2021-11-28 19:20:01 -0500

    Remove \n from buy info

--- FinalProject/src/towers/FireRobot.java
+++ FinalProject/src/towers/FireRobot.java
@@ -10,7 +10,7 @@ public class FireRobot extends Tower {
 
 	public FireRobot(GameScene scene, Vector2 pos) {
 		super(scene, "Flamethrower", pos, 50, 1, 3, 200,
-				new BuyInfo("Flamethrower", "A powerful robot that incinerates\nbloons within a small radius.", 500));
+				new BuyInfo("Flamethrower", "A powerful robot that incinerates bloons within a small radius.", 500));
 		
 		sprite = scene.getGame().getSpriteManager().getSprite("firerobot.png");
 	}
