c539c2132b2b0a78ff45884e0a7cb131517682b2
350076576 <350076576@408D0NEW791JQ22.EDU.YRDSB.NET>
2021-11-29 12:47:28 -0500

    Remove \n from description

--- FinalProject/src/towers/ScissorRobot.java
+++ FinalProject/src/towers/ScissorRobot.java
@@ -9,7 +9,7 @@ public class ScissorRobot extends Tower {
 
 	public ScissorRobot(GameScene scene, Vector2 pos) {
 		super(scene, "ScissorRobot", pos, 80, 1, 4, 1000, 
-				new BuyInfo("Scissor Robot", "Pierces many bloons at once,\nbut needs careful positioning.", 350));
+				new BuyInfo("Scissor Robot", "Pierces many bloons at once, but needs careful positioning.", 350));
 		sprite = scene.getGame().getSpriteManager().getSprite("scissorrobot.png");
 	}
 
