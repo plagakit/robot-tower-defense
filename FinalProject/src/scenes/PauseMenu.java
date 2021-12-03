ee4ac0eb8a96b58e9df1538f2acf4dc03986a859
350076576 <350076576@408D0NEW791JQ22.EDU.YRDSB.NET>
2021-12-03 14:21:49 -0500

    Run the pause menu on a new thread to prevent game from freezing

--- FinalProject/src/scenes/PauseMenu.java
+++ FinalProject/src/scenes/PauseMenu.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import javax.swing.JCheckBox;
 import javax.swing.JOptionPane;
 import javax.swing.JSlider;
+import javax.swing.SwingUtilities;
 
 import general.Settings;
 import graphics.Renderer;
@@ -12,64 +13,83 @@ import graphics.Renderer;
 public class PauseMenu {
 
 	GameScene scene;
-	
-	// First pass allows pause menu to render black screen on game scene then show JOptionPane
-	boolean firstPass;
+	boolean showedDialog;
 	
 	public PauseMenu(GameScene scene) {
 		this.scene = scene;
-		
-		firstPass = true;
+		showedDialog = false;
 	}
-
+	
 	public void update() {
-		if (firstPass)
+
+		if (showedDialog)
 			return;
 		
-		Settings settings = scene.getGame().getSettings();
+		showedDialog = true;
 		
-		JCheckBox autostartCheckbox = new JCheckBox(" Auto-start rounds");
-		autostartCheckbox.setSelected(settings.getAutostart());
 		
-		JSlider volumeSlider = new JSlider(JSlider.HORIZONTAL, 0, 100, 50);
-		volumeSlider.setMajorTickSpacing(50);
-		volumeSlider.setMinorTickSpacing(5);
-		volumeSlider.setPaintLabels(true);
-		volumeSlider.setPaintTicks(true);
-		volumeSlider.setValue(settings.getVolume());
-		
-		Object[] menu = { autostartCheckbox, "\nVolume:", volumeSlider };	
-		Object[] options = {"Back", "Restart", "Exit to Main Menu"};
-		
-		int choice = JOptionPane.showOptionDialog(scene.getGame().getDisplay().getJFrame(),
-			    menu,
-			    "Settings",
-			    JOptionPane.YES_NO_OPTION,
-			    JOptionPane.PLAIN_MESSAGE,
-			    null,
-			    options,
-			    options[0]);
-		
-		settings.setAutostart(autostartCheckbox.isSelected());
-		settings.setVolume(volumeSlider.getValue());
-		
-		if (choice == 0) { // Unpause
-			scene.unpause();
-		}
-		else if (choice == 1) { // Restart
-			scene.onStart();
-		}
-		else if (choice == 2) { // Return to main menu
-			System.exit(0);
-		}
+		// run the joptionpane on a separate thread
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				
+				Settings settings = scene.getGame().getSettings();
+				
+				JCheckBox autostartCheckbox = new JCheckBox(" Auto-start rounds");
+				autostartCheckbox.setSelected(settings.getAutostart());
+				
+				JSlider volumeSlider = new JSlider(JSlider.HORIZONTAL, 0, 100, 50);
+				volumeSlider.setMajorTickSpacing(50);
+				volumeSlider.setMinorTickSpacing(5);
+				volumeSlider.setPaintLabels(true);
+				volumeSlider.setPaintTicks(true);
+				volumeSlider.setValue(settings.getVolume());
+				
+				Object[] menu = { autostartCheckbox, "\nVolume:", volumeSlider };	
+				Object[] options = {"Back", "Restart", "Exit to Main Menu"};
+				
+				int choice = JOptionPane.showOptionDialog(null,
+					    menu,
+					    "Settings",
+					    JOptionPane.YES_NO_OPTION,
+					    JOptionPane.PLAIN_MESSAGE,
+					    null,
+					    options,
+					    options[0]);
+				
+				settings.setAutostart(autostartCheckbox.isSelected());
+				settings.setVolume(volumeSlider.getValue());
+				
+				setChoice(choice);
+			}	
+		});
 	}
 
 	public void render(Renderer r) {		
 		r.setColor(Color.BLACK);
 		r.setTransparency(0.5f);
 		r.fillRect(0, 0, scene.getGame().getWidth(), scene.getGame().getHeight());
+	}
 	
-		firstPass = false;
+	// even though one other pause menu can be run at a time, i put this just in case
+	private synchronized void setChoice(int choice) { 
+		
+		switch (choice) {
+		
+		case 0:
+			scene.unpause();
+			showedDialog = false;
+			break;
+		case 1:
+			scene.onStart();
+			break;
+		case 2:
+			System.exit(0);
+			break;
+		default:
+			showedDialog = false;
+			break;
+		}
 	}
 
 }
