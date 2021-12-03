743a6010f69666245f050e6fea483b6554781d2e
wer8888 <wer8888vg@gmail.com>
2021-12-03 18:02:18 -0500

    Add scissor robot upgrades

--- FinalProject/src/towers/ScissorRobot.java
+++ FinalProject/src/towers/ScissorRobot.java
@@ -8,9 +8,54 @@ import scenes.GameScene;
 public class ScissorRobot extends Tower {
 
 	public ScissorRobot(GameScene scene, Vector2 pos) {
-		super(scene, "ScissorRobot", pos, 80, "scissor.png", 1, 4, 1000, 
+		super(scene, "Scissor Robot", pos, 80, "scissor.png", 1, 4, 1000, 
 				new BuyInfo("Scissor Robot", "Pierces many bloons at once, but needs careful positioning.", 350));
 		sprite = scene.getGame().getSpriteManager().getSprite("scissorrobot.png");
+	
+		upgradePath = new UpgradePath(new Upgrade[][] {
+			{ // Branch 1
+				// Upgrade 1
+				new Upgrade(this, 
+						new BuyInfo("Sharper Blades", "Sharper blades damage and pierce more bloons!", 400)) {
+					@Override
+					public void apply() {
+						tower.pierce += 3;
+						tower.damage += 2;
+					}
+				},
+				// Upgrade2
+				new Upgrade(this, 
+						new BuyInfo("Shurikens", "Allows the robot to shoot razor-sharp ninja stars!", 3000)) {
+					@Override
+					public void apply() {
+						tower.pierce += 20;
+						tower.damage += 10;
+						tower.projectileSprite = "scissorupgrade1.png";
+					}
+				}
+			},
+			{ // Branch 2
+				// Upgrade 1
+				new Upgrade(this, 
+						new BuyInfo("Double Shot", "Allows two scissors to be thrown at once!", 750)) {
+					@Override
+					public void apply() {
+						tower.reloadTime /= 2;
+						tower.pierce += 2;
+					}
+				},
+				// Upgrade 2
+				new Upgrade(this, 
+						new BuyInfo("Chakrams", "Allows the robot to shoot many deadly chakrams that slice bloons with ease!", 3000)) {
+					@Override
+					public void apply() {
+						tower.reloadTime /= 3;
+						tower.pierce += 5;
+						tower.projectileSprite = "scissorupgrade2.png";
+					}
+				}
+			}
+		});
 	}
 
 	@Override
