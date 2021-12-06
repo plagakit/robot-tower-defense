48dc51ed3184ccc989100b9f335376366bd137df
350076576 <350076576@408D0NEW791JQ22.EDU.YRDSB.NET>
2021-12-06 13:33:28 -0500

    Change fire robot description

--- FinalProject/src/towers/FireRobot.java
+++ FinalProject/src/towers/FireRobot.java
@@ -10,7 +10,7 @@ public class FireRobot extends Tower {
 
 	public FireRobot(GameScene scene, Vector2 pos) {
 		super(scene, "Flamethrower", pos, 50, "fire.png", 1, 2, 350,
-				new BuyInfo("Flamethrower", "A powerful robot that incinerates bloons within a small radius.", 500));
+				new BuyInfo("Flamethrower", "A powerful robot that roasts bloons within a small radius.", 500));
 		
 		sprite = scene.getGame().getSpriteManager().getSprite("firerobot.png");
 	
