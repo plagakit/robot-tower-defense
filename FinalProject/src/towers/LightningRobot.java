adb4ebe3c01fd15694853e215f75ce93e248839d
wer8888 <wer8888vg@gmail.com>
2021-12-22 23:47:11 -0500

    Add lightning robot upgrades

--- FinalProject/src/towers/LightningRobot.java
+++ FinalProject/src/towers/LightningRobot.java
@@ -2,6 +2,7 @@ package towers;
 
 import java.awt.Color;
 
+import components.CircleBounds;
 import gameobjects.BuyInfo;
 import general.Vector2;
 import projectiles.Lightning;
@@ -11,6 +12,8 @@ import scenes.GameScene;
 
 public class LightningRobot extends Tower {
 
+	private Color currentColor = Color.MAGENTA;
+	
 	public LightningRobot(GameScene scene, Vector2 pos) {
 		super(scene, "Tesla Robot", pos, 70, 1000, 
 				new ProjectileData(new Lightning(Color.MAGENTA, 5), 1, 5, null, 500), 
@@ -18,6 +21,52 @@ public class LightningRobot extends Tower {
 		
 		sprite = scene.getGame().getSpriteManager().getSprite("lightningrobot.png");
 
+		upgradePath = new UpgradePath(new Upgrade[][] {
+			{ // Branch 1
+				// Upgrade 1
+				new Upgrade(this, 
+						new BuyInfo("Stronger Charge", "Strengthens the electromagnetism of the lightning, allowing it to hit more bloons!", 700)) {
+					@Override
+					public void apply() {
+						tower.projectileData.addPierce(5);
+						tower.projectileData.addDamage(2);
+					}
+				},
+				// Upgrade2
+				new Upgrade(this, 
+						new BuyInfo("Plasma Beams", "Chaotic plasma beams melt right through bloons!", 4000)) {
+					@Override
+					public void apply() {
+						currentColor = new Color(59, 0, 122);
+						tower.projectileData.addDamage(10);
+						tower.projectileData.addPierce(10);
+					}
+				}
+			},
+			{ // Branch 2
+				// Upgrade 1
+				new Upgrade(this, 
+						new BuyInfo("AC/DC Current", "Increases both range and fire speed!", 600)) {
+					@Override
+					public void apply() {
+						tower.range = new CircleBounds(tower, 80);
+						reloadTime /= 1.5f;
+					}
+				},
+				// Upgrade 2
+				new Upgrade(this, 
+						new BuyInfo("Quantum Elec.", "New theoretical \"quantum electricity\" phases through the subatomic bloon particles!", 8000)) {
+					@Override
+					public void apply() {
+						tower.projectileData.addPierce(5);
+						reloadTime /= 1.5f;
+						currentColor = new Color(187, 255, 0);
+						tower.projectileData.setProjectileType(new Lightning(currentColor, tower.projectileData.getPierce()));
+					}
+				}
+			}
+		});
+		
 	}
 
 	@Override
