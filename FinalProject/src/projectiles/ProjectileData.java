47236f1e3a5ee1a1c289ec71f0c3661fa90511e1
wer8888 <wer8888vg@gmail.com>
2021-12-25 19:38:37 -0500

    Add projectile effects to projectile data

--- FinalProject/src/projectiles/ProjectileData.java
+++ FinalProject/src/projectiles/ProjectileData.java
@@ -7,13 +7,15 @@ public class ProjectileData {
 	private int pierce;
 	private String projectileSpritePath;
 	private int despawnTime;
+	private ProjectileEffect[] effects;
 	
-	public ProjectileData(Projectile projectileType, int damage, int pierce, String projectileSpritePath, int despawnTime) {
+	public ProjectileData(Projectile projectileType, int damage, int pierce, String projectileSpritePath, int despawnTime, ProjectileEffect... effects) {
 		this.projectileType = projectileType;
 		this.damage = damage;
 		this.pierce = pierce;
 		this.projectileSpritePath = projectileSpritePath;
 		this.despawnTime = despawnTime;
+		this.effects = effects;
 	}
 	
 	public Projectile getProjectileType() { return projectileType; }
@@ -31,4 +33,6 @@ public class ProjectileData {
 	public int getDespawnTime() { return despawnTime; }
 	public void setDespawnTime(int despawnTime) { this.despawnTime = despawnTime; }
 	
+	public ProjectileEffect[] getEffects() { return effects; }
+	public void setEffects(ProjectileEffect... effects) { this.effects = effects; }
 }
