6c35f4387263101ee3823c5fb95cc8c1cb05856a
350076576 <350076576@408D0NEW791JQ22.EDU.YRDSB.NET>
2022-01-20 14:34:13 -0500

    Add comments to input manager

--- FinalProject/src/game/InputManager.java
+++ FinalProject/src/game/InputManager.java
@@ -9,6 +9,11 @@ import java.awt.event.MouseMotionListener;
 /** The InputManager class is used to register key press, mouse movement, and mouse clicks and then store them for use in game. */
 public class InputManager implements KeyListener, MouseListener, MouseMotionListener {
 
+	/* i made this class at the start of the project, intending to use 
+	 * key presses. i spent a long time creating the keys, justPressed, and 
+	 * cantPress system but ironically i found later that i only needed the mouse
+	 * for input. */
+	
 	private Game game;
 	
 	private final int KEY_SIZE = 256;
@@ -27,7 +32,7 @@ public class InputManager implements KeyListener, MouseListener, MouseMotionList
 		mousePos = new Vector2(0, 0);
 	}
 	
-	/** */
+	/** Register key presses. */
 	public void update() {
 		for (int i = 0; i < KEY_SIZE; i++) {
 			if (cantPress[i] && !keys[i])
@@ -105,8 +110,10 @@ public class InputManager implements KeyListener, MouseListener, MouseMotionList
 		//System.out.println("moved");
 	}
 	
+	/** Returns the mouse position respective to the window and its size. */
 	public Vector2 getAbsoluteMousePos() { return mousePos; }
 	
+	/** Returns the mouse position respective to the game and its size (ignoring displaySize). */
 	public Vector2 getMousePos() { 
 		return new Vector2(mousePos.x / game.getScale(), mousePos.y / game.getScale());
 	}
